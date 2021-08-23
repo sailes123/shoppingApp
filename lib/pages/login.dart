3d238332f820e059c948087f58765d8193dd19c7
Sailes123 <saileshbajgain056@gmail.com>
2021-08-23 17:52:39 +0545

    login Page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -11,11 +11,44 @@ class LogIn extends StatelessWidget {
             "assets/images/login.png",
             fit: BoxFit.scaleDown,
           ),
+          SizedBox(
+            height: 40,
+          ),
           Text("Welcome To Login Page",
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
               )),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 52),
+            child: Column(
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(
+                  hintText: "Enter username",
+                  labelText: "Username",
+                )),
+                TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password",
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("LogIn"),
+                  onPressed: () {
+                    print("Hello my login bro");
+                  },
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
