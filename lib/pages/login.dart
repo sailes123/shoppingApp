1d307be5542502ee42c8c028787e6dd0fae4fe21
Sailes123 <saileshbajgain056@gmail.com>
2021-08-30 23:03:23 +0545

    validation to login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,61 +8,83 @@ class LogIn extends StatefulWidget {
 
 class _LogInState extends State<LogIn> {
   String name = "";
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login.png",
-              fit: BoxFit.scaleDown,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text("Welcome To Login Page, $name",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                )),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 52),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter username",
-                      labelText: "Username",
+        child: Form(
+          key: formkey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login.png",
+                fit: BoxFit.scaleDown,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text("Welcome To Login Page, $name",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 52),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter username",
+                        labelText: "Username",
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Username should be filled";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {});
-                    },
-                  ),
-                  TextFormField(
+                    TextFormField(
                       obscureText: true,
                       decoration: InputDecoration(
                         hintText: "Enter password",
                         labelText: "Password",
-                      )),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  RaisedButton(
-                    child: Text("LogIn"),
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoutes);
-                    },
-                  )
-                ],
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Password should be filled";
+                        } else if (value.length < 6) {
+                          return "Password length must be six";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    RaisedButton(
+                      child: Text("LogIn"),
+                      onPressed: () {
+                        if (formkey.currentState.validate()) {
+                          Navigator.pushNamed(context, MyRoutes.homeRoutes);
+                        }
+                      },
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
